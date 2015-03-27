d022cc901a005dffd85506f6eff2769551936aef
Jeffery Wilkins <djcanadianjeff@gmail.com>
2015-03-27 14:32:41 -0700

    Logs Handled By Script Not Twisted

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#date '+%Y-%m-%d %H:%M:%S%z' | awk '{print $1,$2" [-] Starting honeywrt IDS in the background..."}'
-#cd $(dirname $0)
-#twistd -y honeywrt.tac -l log/honeywrt.log --pidfile honeywrt.pid
-
-date '+%Y-%m-%d %H:%M:%S%z' | awk '{print $1,$2" [-] Starting honeywrt IDS in the foreground..."}'
+### Background
 cd $(dirname $0)
-twistd -y honeywrt.tac -n
+twistd -y honeywrt.tac --pidfile honeywrt.pid
+
+### Forground
+#cd $(dirname $0)
+#twistd -y honeywrt.tac -n
